1b7f43829cbc768ee7391b9b8c16c8c8d308581b
missmoe <tmoe@bfgcom.com>
2018-07-30 18:02:47 -0400

    starting the game coding

--- js/app.js
+++ js/app.js
@@ -24,12 +24,39 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.x = 205;
+  this.y = 375;
+  this.update = function(){
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+  this.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+  this.handleInput = function(direction) {
+      var move = 0;
+      if(direction === 'up') {
+          this.y -= 83;
+      } else if(direction === 'down') {
+          this.y += 83;
+      } else if(direction === 'left') {
+          this.x -= 100;
+      } else {
+          this.x += 100;
+      }
+  };
+  this.selectPlayer = function(image) {
+      this.sprite = image;
+  };
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
